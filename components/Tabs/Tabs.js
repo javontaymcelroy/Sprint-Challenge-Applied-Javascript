a9165142e96f0002ad563a2480df9cc9cb9c88d3
Javontay McElroy <dinomightnetwork@gmail.com>
2019-02-01 10:37:02 -0600

    Working on SelectTab

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,32 +1,21 @@
 class TabLink {
   constructor(tabElement){
-    // assign this.tabElement to the tabElement DOM reference
-    // this.tabElement;
-    
-    // Get the `data-tab` value from this.tabElement and store it here
-    // this.tabData = ; 
-    
-    // We need to find out if a user clicked 'all' cards or a specific category.  Follow the instructions below to accomplish this task:    
-    /*
-    // Check to see if this.tabData is equal to 'all'
-    if(){
-      // If `all` is true, select all cards regardless of their data attribute values
-      // this.cards = ;
+
+    this.tabElement = element;
+    this.tabData = this.element.dataset.tab;
+  
+    if(this.tabData === 'all'){
+      this.cards = document.querySelectorAll('.card');
     } else {
-      // else if `all` is false, only select the cards with matching this.tabData values
-      // this.cards = ;
-    }
-    /*
+      this.cards = document.querySelectorAll(`.card[data-tab="${data}"]`);
 
-     // Map over the newly converted NodeList we just created in our if statement above. Convert each this.cards element into a new instance of the TabCard class. Pass in a card object to the TabCard class. 
-    // this.cards = Array.from(this.cards).map();
+      this.cards = Array.from(this.cards).map(card => new TabCard(card));
 
-    // Add a click event that invokes this.selectTab
-    // this.tabElement.addEventListener();
+      this.tabElement.addEventListener('click', () => this.selectTab());
   }
 
   selectTab(){
-
+    const tabs = document.querySelectorAll('.tab');
     // Select all elements with the .tab class on them
     // const tabs = document.querySelectorAll();
     
@@ -69,4 +58,4 @@ class TabCard {
 
 */
 let tabs = document.querySelectorAll('.tabs');
-tabs.forEach(link => new TabLink(link));
+tabs.forEach(tab => new TabLink(tab));
