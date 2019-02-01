c3bf933e6360bf19b6bbbe6641db951e2b4a1a5a
Javontay McElroy <dinomightnetwork@gmail.com>
2019-02-01 10:42:38 -0600

    I think I am finished with SelectTab...not sure because nothing works...

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -16,23 +16,17 @@ class TabLink {
 
   selectTab(){
     const tabs = document.querySelectorAll('.tab');
-    // Select all elements with the .tab class on them
-    // const tabs = document.querySelectorAll();
-    
-    // Iterate through the NodeList removing the .active-tab class from each element
-    // tabs.forEach()
 
-    // Select all of the elements with the .card class on them
-    // const cards = ;
+    tabs.forEach(tab => tab.classList.remove('active-tab'));
 
-    // Iterate through the NodeList setting the display style each one to 'none'
-    // cards.forEach()
-    
-    // Add a class of ".active-tab" to this.tabElement
-    // this.tabElement;
-  
-    // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class. Just un-comment the code and study what is happening here.
-    // this.cards.forEach(card => card.selectCard());
+    const tabs = document.querySelectorAll('.card');
+
+    cards.forEach(card => card.style.display = 'none');
+
+    this.tabElement.classList.add('active-tab');
+
+    this.cards.forEach(card => card.selectCard());
+
   }
 }
 
@@ -48,14 +42,5 @@ class TabCard {
 
 }
 
-/* START HERE: 
-
-- Select all classes named ".tab" and assign that value to the tabs variable
-
-- With your selection in place, now chain a .forEach() method onto the tabs variable to iterate over the DOM NodeList
-
-- In your .forEach() method's callback function, return a new instance of TabLink and pass in each tab as a parameter
-
-*/
 let tabs = document.querySelectorAll('.tabs');
 tabs.forEach(tab => new TabLink(tab));
